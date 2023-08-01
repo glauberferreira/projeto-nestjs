aec0803313c4e3f6f85b03c40c0831c5e6dc85b7
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-01 15:36:22 -0300

    retorna um array de gatos na rota /cats

--- src/cats/cats.controller.js
+++ src/cats/cats.controller.js
@@ -1,11 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 
+const GATOS = [
+    {
+        id: 1,
+        nome: "Franeudos",
+        corOlhos: "verde",
+        raca: "sphynx"
+    },
+    {
+        id: 2,
+        nome: "Chico",
+        corOlhos: "azul",
+        raca: "siamês"
+    },
+    {
+        id: 3,
+        nome: "Chambaril",
+        corOlhos: "preto",
+        raca: "munchkin"
+    }
+];
+
 @Controller('cats')
 export class CatsController {
 
     @Get()
     findAll() {
-        return "Retorna todos os gatos";
+        return GATOS;
     }
 
 }
