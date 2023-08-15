932fee5a76f0f63583edfd59c3797b2f4ad619de
Glauber Ferreira <glauber.ferreira@nees.ufal.br>
2023-08-15 16:30:26 -0300

    ajusta a rota DELETE /cats/:id para usar serviço

--- src/cats/cats.service.js
+++ src/cats/cats.service.js
@@ -17,4 +17,12 @@ export class CatsService {
     findById(id) {
         return this.cats.find(cat => cat.id == id);
     }
+
+    findIndexById(id) {
+        return this.cats.findIndex(cat => cat.id == id);
+    }
+
+    deleteByIndex(index){
+        this.cats.splice(index, 1);
+    }
 }
